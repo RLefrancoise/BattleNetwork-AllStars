17319497c2f73adcbb19f19febe62e1001cc4ccb
supervlad35 <supervlad35@19a373f5-5864-4f38-81d6-406046a000cc>
2012-01-12 17:11:35 +0000

    Poursuite de la programmation du Character Select

--- Scene/MMBNCharacterSelect.cpp
+++ Scene/MMBNCharacterSelect.cpp
@@ -10,17 +10,41 @@ CharacterSelectInfo::CharacterSelectInfo()
 	m_body = NULL;
 }
 
-CharacterSelectInfo::CharacterSelectInfo(string name, OSL_IMAGE* icon, OSL_IMAGE* body)
+CharacterSelectInfo::CharacterSelectInfo(string name, OSL_IMAGE* icon, OSL_IMAGE* body, Vector2i pos)
 {
 	m_name = name;
 	m_icon = icon;
 	m_body = body;
+	m_position = pos;
+}
+
+CharacterSelectInfo::~CharacterSelectInfo()
+{
+}
+
+string CharacterSelectInfo::GetName()
+{
+	return m_name;
+}
+
+OSL_IMAGE* CharacterSelectInfo::GetIcon()
+{
+	return m_icon;
+}
+
+OSL_IMAGE* CharacterSelectInfo::GetBody()
+{
+	return m_body;
+}
+
+Vector2i CharacterSelectInfo::GetPosition()
+{
+	return m_position;
 }
 
 
-
-
-
+//int	MMBNCharacterSelect::CHARA_PER_LINE = 5			;
+//int	MMBNCharacterSelect::CHARA_ICON_SIZE = 32		;
 
 
 MMBNCharacterSelect::MMBNCharacterSelect()
@@ -47,6 +71,9 @@ int	MMBNCharacterSelect::Run()
 
 void MMBNCharacterSelect::Initialize()
 {
+	CHARA_PER_LINE = 5			;
+	CHARA_ICON_SIZE = 38		;
+
 	//backgrounds
 	m_bg				= Animation::Load("Backgrounds/CharacterSelect")		;
 	m_plug_in			= Animation::Load("Backgrounds/PlugIn", false, false)	;
@@ -58,74 +85,73 @@ void MMBNCharacterSelect::Initialize()
 	//pictures
 	m_chara_frame		= ImgManager::GetImage("System/Menus/CharacterSelect/chara_frame.png");
 	
-	
-	//characters
-	ifstream in_actors( "Actors/actors.txt" , ifstream::in);
 		
-	if(!in_actors.good())
-	{
-		LOG("Impossible de trouver le fichier Actors/actors.txt");
-		oslQuit();
-	}
+	//character data
+	vector<string>& v = GameSystem::GetActorsNames();
 	
-	string line;
-	while(getline(in_actors, line))
-	{
-		vector<string> v = StringUtils::Split(line, ":");
+	boost::array<CharactersInfo::index,1> shape = { { v.size() } };
+	CharactersInfoPtr ptr(new CharactersInfo(shape));
+	m_characters.swap(ptr);
+	
+	m_chara_nb = v.size();
+	
+	int begin_x = 240 - (CHARA_PER_LINE * CHARA_ICON_SIZE) / 2;
+	//int begin_y = 136 - ( ( m_chara_nb % CHARA_PER_LINE) * CHARA_ICON_SIZE ) / 2;
+	int begin_y = 136 - ( ( m_chara_nb % CHARA_PER_LINE) * CHARA_ICON_SIZE ) / 2;
+	
+	#ifdef _DEBUG
+	ostringstream oss(ostringstream::out);
+	oss << "begin_x: " << begin_x << " begin_y: " << begin_y;
+	LOG(oss.str())
+	#endif
+	
+	#ifdef _DEBUG
+	LOG("Load characters")
+	#endif
 		
+	//characters data
+	for(unsigned int i(0) ; i < m_chara_nb ; i++)
+	{
 		#ifdef _DEBUG
-		LOG("Load character: " + v[0])
+		LOG("Found character: " + v[i])
 		#endif
 
-		CharacterSelectInfoPtr csi(new CharacterSelectInfo(v[0], ImgManager::GetImage("Actors/" + v[0] + "/icon.png"), NULL));
+		Vector2i pos;
+		pos.x = begin_x + CHARA_ICON_SIZE * (i % CHARA_PER_LINE);
+		pos.y = begin_y + CHARA_ICON_SIZE * (i / CHARA_PER_LINE);
 		
-		m_characters.push_back(csi);
+		CharacterSelectInfoPtr ptr(new CharacterSelectInfo (v[i], ImgManager::GetImage(string("Actors/") + v[i] + string("/icon.png")), NULL, pos));
+		CharactersInfo& ci = *(m_characters.get());
+		ci[i].swap(ptr);
 		
-		/*m_characters.back()->m_name = v[0];
-		m_characters.back()->m_icon = ImgManager::GetImage("Actors/" + v[0] + "/icon.png");
-		m_characters.back()->m_body = NULL;*/
 		#ifdef _DEBUG
 		LOG("Character loaded")
 		#endif
 	}
 	
-	in_actors.close();
-		
-	//characters data
-	/*for(unsigned int i(0) ; i < GameSystem::GetActorsNames().size() ; i++)
-	{
-		#ifdef _DEBUG
-		LOG("Found character: " + GameSystem::GetActorsNames()[i])
-		#endif
-
-		m_characters.push_back(csi);
-		
-		m_characters.back().name = GameSystem::GetActorsNames()[i];
-		m_characters.back().icon = ImgManager::GetImage("Actors/" + GameSystem::GetActorsNames()[i] + "/icon.png");
-		m_characters.back().body = NULL;
-	}*/
+	//texts
+	//m_title.SetFont(GameSystem::GetBattleFont());
+	//m_title = "Character Select";
+	//m_title.SetPosition(240 - m_title.GetStringWidth() / 2, 5);
 	
-	/*int begin_x = 240 - (CHARA_PER_LINE * CHARA_ICON_SIZE) / 2;
-	int begin_y = 136 - ( (m_characters.size() % CHARA_PER_LINE) * CHARA_ICON_SIZE ) / 2;
-	
-	for(unsigned int i(0) ; i < m_characters.size() ; ++i)
-	{
-		Vector2i v;
-		v.x = begin_x + CHARA_ICON_SIZE * (i % CHARA_PER_LINE);
-		v.y = begin_y + CHARA_ICON_SIZE * (i / CHARA_PER_LINE);
-		m_chara_positions.push_back(v);
-	}*/
 	
 	//variables
-	m_current_bgm		= 0														;
-	m_can_plug_in		= false													;
+	m_current_actor		= 0																					;
+	(m_chara_nb > CHARA_PER_LINE) ? m_current_enemy = CHARA_PER_LINE - 1 : m_current_enemy = m_chara_nb - 1	;
+	m_current_bgm		= 0																					;
+	m_can_plug_in		= false																				;
 }
 
 void MMBNCharacterSelect::Destroy()
 {
 	SndManager::StopBGM(0);
+	/*for(unsigned int i(0) ; i < m_characters.size() ; ++i)
+	{
+		if(m_characters[i]) delete m_characters[i];
+	}*/
+	
+	//if(m_characters) delete[] m_characters;
 }
-
 
 int MMBNCharacterSelect::Update()
 {
@@ -159,11 +185,36 @@ void MMBNCharacterSelect::Display()
 	m_bg->Update();
 	m_bg->Display();
 	
+	//title
+	MMBNString title;
+	title.SetFont(GameSystem::GetBattleFont());
+	title = "Character Select";
+	title.SetPosition(240 - title.GetStringWidth() / 2, 5);
+	title.Display();
+	
 	//characters
-	/*for(unsigned int i(0) ; i < m_characters.size() ; ++i)
+	CharactersInfo& ci = *(m_characters.get());
+	
+	for(unsigned int i(0) ; i < m_chara_nb ; ++i)
 	{
-		oslDrawImageXY(m_characters[i]->m_icon, m_chara_positions[i].x, m_chara_positions[i].y);
-	}*/
+		CharacterSelectInfoPtr& csi = ci[i];
+		//frame
+		oslDrawImageXY(m_chara_frame, csi->GetPosition().x, csi->GetPosition().y);
+		//icon
+		oslDrawImageXY(csi->GetIcon(), csi->GetPosition().x + 3, csi->GetPosition().y + 3);
+	}
+	
+	//cursors
+	//actor
+	CharacterSelectInfoPtr& csi_a = ci[m_current_actor];
+	m_actor_cursor->SetPosition(csi_a->GetPosition().x + CHARA_ICON_SIZE / 2, csi_a->GetPosition().y + CHARA_ICON_SIZE / 2);
+	m_actor_cursor->Update();
+	m_actor_cursor->Display();
+	//enemy
+	CharacterSelectInfoPtr& csi_e = ci[m_current_enemy];
+	m_enemy_cursor->SetPosition(csi_e->GetPosition().x + CHARA_ICON_SIZE / 2, csi_e->GetPosition().y + CHARA_ICON_SIZE / 2);
+	m_enemy_cursor->Update();
+	m_enemy_cursor->Display();
 	
 	SndManager::PlayBGM("Digital_Strider.bgm", 0, true);
 
